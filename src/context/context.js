213612aa82e4c99a002c7b724dffe60f4707651e
cawauchi6204 <cawauchi6204@gmail.com>
2020-11-27 20:33:28 +0900

    context apiの実装完了

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = createContext()
+
+const GithubProvider = ({ chirdren }) => {
+  return <GithubContext.Provider value={'hello'}>
+    {chirdren}
+  </GithubContext.Provider>
+}
+
+export { GithubProvider, GithubContext }
+
